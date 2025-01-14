a705245ca92b78be5498ed76a4fbf4c0e539af15
clm <limin_chu@163.com>
2025-01-14 16:17:53 +0800

    useRef的使用

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,8 +1,9 @@
 // import List1 from '@/List1'
 // import StateDemo1 from '@/StateDemo1'
 // import StateDemo2 from '@/StateDemo2'
-import List2 from '@/List2'
+// import List2 from '@/List2'
 // import ImmerDemo from '@/ImmerDemo'
+import UseRefDemo from '@/UseRefDemo'
 
 export default function App() {
   return (
@@ -10,8 +11,9 @@ export default function App() {
       {/* <List1 /> */}
       {/* <StateDemo1 /> */}
       {/* <StateDemo2 /> */}
-      <List2 />
+      {/* <List2 /> */}
       {/* <ImmerDemo /> */}
+      <UseRefDemo />
     </>
   )
 }

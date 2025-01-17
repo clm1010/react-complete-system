1370520dd7c91ec8b92969d78a01e427f230c9a7
clm <limin_chu@163.com>
2025-01-17 17:04:08 +0800

    问卷List

--- react-app-project/src/App.tsx
+++ react-app-project/src/App.tsx
@@ -1,25 +1,9 @@
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
+import List from './pages/List'
+export default function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<h1 style={{ backgroundColor: 'yellow' }}>问卷</h1>
+			<List />
 		</div>
 	)
 }
-
-export default App
---
+++ react-app-project/src/pages/List.tsx
@@ -0,0 +1,74 @@
+import type { FC } from 'react'
+import { useState } from 'react'
+import QuestionCard from '../components/QuestionCard'
+import styles from './List.module.scss'
+
+const rawQuestionList = [
+	{
+		_id: 'q1',
+		title: '问卷1',
+		isPublished: false,
+		isStar: false,
+		answerCount: 5,
+		createdAt: '5月10日 13:23'
+	},
+	{
+		_id: 'q2',
+		title: '问卷2',
+		isPublished: true,
+		isStar: false,
+		answerCount: 5,
+		createdAt: '5月11日 13:23'
+	},
+	{
+		_id: 'q3',
+		title: '问卷3',
+		isPublished: false,
+		isStar: false,
+		answerCount: 5,
+		createdAt: '5月12日 13:23'
+	},
+	{
+		_id: 'q4',
+		title: '问卷4',
+		isPublished: true,
+		isStar: false,
+		answerCount: 5,
+		createdAt: '5月13日 13:23'
+	}
+]
+
+const List: FC = () => {
+	const [questionList, setQuestionList] = useState(rawQuestionList)
+
+	return (
+		<>
+			<div className={styles.header}>
+				<div className={styles.left}>
+					<h3>我的问卷</h3>
+				</div>
+				<div className={styles.right}>(搜索)</div>
+			</div>
+			<div className={styles.content}>
+				{questionList.map((question) => {
+					const { _id } = question
+					return (
+						<QuestionCard
+							key={_id}
+							// {...question} 也可以这么写，解构
+							_id={question._id}
+							title={question.title}
+							isPublished={question.isPublished}
+							isStar={question.isStar}
+							answerCount={question.answerCount}
+							createdAt={question.createdAt}
+						/>
+					)
+				})}
+			</div>
+			<div className={styles.footer}>footer</div>
+		</>
+	)
+}
+
+export default List

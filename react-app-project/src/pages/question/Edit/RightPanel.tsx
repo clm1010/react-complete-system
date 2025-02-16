5deca57b3555e1abe27e10561254de531be1d519
clm1010 <limin_chu@163.com>
2025-02-17 00:00:40 +0800

    动态切换右侧tabs-根据选中组件动态切换属性和页面

--- react-app-project/src/pages/question/Edit/RightPanel.tsx
+++ react-app-project/src/pages/question/Edit/RightPanel.tsx
@@ -1,12 +1,33 @@
 import type { FC } from 'react'
+import { useState, useEffect } from 'react'
 import { Tabs } from 'antd'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import ComponentProp from './ComponentProp'
+import PageSetting from './PageSetting'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+
+// TS 枚举类型，用于标识选项卡的 key
+enum TAB_KEYS {
+	PROP_KEY = 'prop',
+	SETTING_KEY = 'setting'
+}
 
 const RightPanel: FC = () => {
+	const [activeKey, setActiveKey] = useState(TAB_KEYS.PROP_KEY)
+	const { selectedId } = useGetComponentInfo()
+
+	// 切换选中组件时，自动切换到属性面板
+	useEffect(() => {
+		if (selectedId) {
+			setActiveKey(TAB_KEYS.PROP_KEY)
+		} else {
+			setActiveKey(TAB_KEYS.SETTING_KEY)
+		}
+	}, [selectedId])
+
 	const tabsItems = [
 		{
-			key: 'prop',
+			key: TAB_KEYS.PROP_KEY,
 			label: (
 				<span>
 					<FileTextOutlined />
@@ -16,17 +37,17 @@ const RightPanel: FC = () => {
 			children: <ComponentProp />
 		},
 		{
-			key: 'setting',
+			key: TAB_KEYS.SETTING_KEY,
 			label: (
 				<span>
 					<SettingOutlined />
 					页面设置
 				</span>
 			),
-			children: <div>页面设置</div>
+			children: <PageSetting />
 		}
 	]
-	return <Tabs defaultActiveKey="prop" items={tabsItems}></Tabs>
+	return <Tabs activeKey={activeKey} items={tabsItems}></Tabs>
 }
 
 export default RightPanel
